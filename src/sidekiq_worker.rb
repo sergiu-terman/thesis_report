b186648f0c8c5ca1307ac98a93c3b8c627624f1b
Sergiu Terman <terman.sergiu@gmail.com>
2015-05-30 01:19:30 +0300

    mor polishing

--- src/sidekiq_worker.rb
+++ src/sidekiq_worker.rb
@@ -3,11 +3,7 @@ class MapReduceWorker
 
   def perform(word, token)
     message = {channel: "/messages/#{token}", data: {message: "#{word} request was analyzed", type: "success"}}
-    begin
-      MongoHelper.new.frequency(word, token)
-    rescue => e
-      message[:data] = {message: "An error occurred while trying to analyze: #{word}", type: "danger"}
-    end
+    MongoHelper.new.frequency(word, token)
     uri = URI.parse("http://localhost:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
